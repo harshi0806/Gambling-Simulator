0a742c424420f60fbb0ab60e824fdcd69ea42bcf
harshi0806 <harshitashrivatav121@gmail.com>
2020-07-30 15:37:58 +0530

    added:gambleUC-1

---
+++ GamblingSimulation.java
@@ -0,0 +1,13 @@
+package GamblingSimulation;
+
+public class GamblingSimulation {
+
+	//Initialize the static variables Stake Amount for a day and bet amount
+	private static final int STAKE_AMOUNT = 100;
+	private static final int BET_AMOUNT = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Gamble Zone!! Let's get started.............");
+	}
+}
+
